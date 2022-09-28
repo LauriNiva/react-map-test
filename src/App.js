467467bfc5277aa6fc137755a653e0834b78432b
Lauri Niva <niva.lauri@gmail.com>
2022-09-28 18:27:02 +0300

    Checkbox eri markkereille

--- src/App.js
+++ src/App.js
@@ -19,9 +19,16 @@ function App() {
 
   const [map, setMap] = useState(null);
 
+  const [mapVisible, setMapVisible] = useState(true);
+
   const [clickedLocations, setClickedLocations] = useState([]);
+  const [centerLocations, setCenterLocations] = useState([]);
+
+  const [showClickedMarkers, setShowClickedMarkers] = useState(true);
+  const [showCenterMarkers, setShowCenterMarkers] = useState(true);
+
   const [canClick, setCanClick] = useState(false);
-  console.log('clickedLocations', clickedLocations);
+  // console.log('clickedLocations', clickedLocations);
 
   const MapClickComponent = () => {
     useMapEvents({
@@ -42,7 +49,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      <div className="sidebar">
         <button
           className="toggle-button"
           onClick={() => setCanClick(!canClick)}
@@ -54,38 +61,77 @@ function App() {
             const center = map.getCenter();
             console.log('Added center location', center);
 
-            setClickedLocations(
-              clickedLocations.concat([[center.lat, center.lng]])
+            setCenterLocations(
+              centerLocations.concat([[center.lat, center.lng]])
             );
           }}
           className="add-button"
         >
           add center
         </button>
+        <button
+          className="toggle-map-button "
+          onClick={() => setMapVisible(!mapVisible)}
+        >
+          {mapVisible ? 'hide map' : 'show map'}
+        </button>
+        -markers-
+        <div>
+          <input
+            type="checkbox"
+            id="clicked"
+            onChange={() => setShowClickedMarkers(!showClickedMarkers)}
+            checked={showClickedMarkers}
+          />
+          <label htmlFor="clicked">Clicked</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            id="center-checkbox"
+            onChange={() => setShowCenterMarkers(!showCenterMarkers)}
+            checked={showCenterMarkers}
+          />
+          <label htmlFor="center-checkbox">Center</label>
+        </div>
       </div>
-      <MapContainer
-        center={[64.07391245239761, 24.53362472782081]}
-        zoom={20}
-        scrollWheelZoom={false}
-        ref={setMap}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <MapClickComponent />
-        <span className="crosshair">+</span>
-        {clickedLocations.map((location) => (
-          <Marker key={location[0]} position={location}>
-            <Popup>
-              New marker at <br />
-              {location[0]}
-              <br />
-              {location[1]}
-            </Popup>
-          </Marker>
-        ))}
-      </MapContainer>
+      {mapVisible && (
+        <MapContainer
+          center={[64.07391245239761, 24.53362472782081]}
+          zoom={20}
+          scrollWheelZoom={false}
+          ref={setMap}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <MapClickComponent />
+          <span className="crosshair">+</span>
+          {showClickedMarkers &&
+            clickedLocations.map((location) => (
+              <Marker key={location[0]} position={location}>
+                <Popup>
+                  New marker at <br />
+                  {location[0]}
+                  <br />
+                  {location[1]}
+                </Popup>
+              </Marker>
+            ))}
+          {showCenterMarkers &&
+            centerLocations.map((location) => (
+              <Marker key={location[0]} position={location}>
+                <Popup>
+                  New marker at <br />
+                  {location[0]}
+                  <br />
+                  {location[1]}
+                </Popup>
+              </Marker>
+            ))}
+        </MapContainer>
+      )}
     </div>
   );
 }
